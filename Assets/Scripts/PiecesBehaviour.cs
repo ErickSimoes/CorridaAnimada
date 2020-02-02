b7a640c9c80137d37c8a6f9a2e36b60ce9ec69a2
Erick Simões <erick.simoes@live.com>
2020-02-02 09:22:49 -0300

    Sketch of piece movimentation

---
+++ Assets/Scripts/PiecesBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiecesBehaviour : MonoBehaviour {
+    public RectTransform pieces;
+    public RectTransform referencePieces;
+    float yDiference = 0;
+
+    void Start() {
+        yDiference = pieces.position.y - referencePieces.position.y;
+    }
+
+    void Update() {
+        Vector2 fixedPosition = new Vector2(referencePieces.position.x, referencePieces.position.y + yDiference);
+        pieces.position = Vector2.MoveTowards(pieces.position, fixedPosition, 1f);
+    }
+}
