1a777d3c66e5ee64608b04514ce1172431e71836
Erick Simões <erick.simoes@live.com>
2020-02-09 16:36:41 -0300

    Automatic object recognition system for position references

--- Assets/Scripts/PiecesBehaviour.cs
+++ Assets/Scripts/PiecesBehaviour.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class PiecesBehaviour : MonoBehaviour {
     public RectTransform piece;
-    public RectTransform[] referencePieces;
+    public GameObject pieceReferenceGroup;
+    RectTransform[] referencePieces;
     int pieceReference = 0;
 
     void Start() {
-        
+        int numPieces = pieceReferenceGroup.transform.childCount;
+        referencePieces = new RectTransform[numPieces];
+
+        for (int i = 0; i < numPieces; i++) {
+            referencePieces.SetValue(pieceReferenceGroup.transform.GetChild(i), i);
+        }
     }
 
     void Update() {
