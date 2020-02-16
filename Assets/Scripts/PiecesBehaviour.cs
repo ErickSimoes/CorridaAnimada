ab71fed2d49841d21e986fbbe5351370198e33b7
Erick Simões <erick.simoes@live.com>
2020-02-16 16:42:10 -0300

    Enable VictoryPanel when a piece reaches the end
    Resolves #1

--- Assets/Scripts/PiecesBehaviour.cs
+++ Assets/Scripts/PiecesBehaviour.cs
@@ -6,6 +6,7 @@ public class PiecesBehaviour : MonoBehaviour {
     public RectTransform piece;
     public GameObject pieceReferenceGroup;
     RectTransform[] referencePieces;
+    static GameObject VictoryPanel;
     int pieceReference = 0;
 
     void Start() {
@@ -14,6 +15,12 @@ public class PiecesBehaviour : MonoBehaviour {
 
         for (int i = 0; i < numPieces; i++) {
             referencePieces.SetValue(pieceReferenceGroup.transform.GetChild(i), i);
+        }
+
+        
+        if (!VictoryPanel) {
+            VictoryPanel = GameObject.FindGameObjectWithTag("VictoryPanel");
+            VictoryPanel.SetActive(false);
         }
     }
 
@@ -25,7 +32,10 @@ public class PiecesBehaviour : MonoBehaviour {
         if (referencePieces.Length - 1 > pieceReference) {
             pieceReference++;
         }
-        
+
+        if (pieceReference == referencePieces.Length - 1) {
+            VictoryPanel.SetActive(true);
+        }
     }
 
 }
