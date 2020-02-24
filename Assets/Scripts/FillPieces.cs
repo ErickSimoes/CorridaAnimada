407f33c96d3033a0ec8e5b00f88e72ea7748a4b4
Erick Simões <erick.simoes@live.com>
2020-02-24 10:18:48 -0300

    Implementation of fill pieces in main scene

---
+++ Assets/Scripts/FillPieces.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FillPieces : MonoBehaviour {
+
+    public Sprite[] sprites;
+    Sprite[] spriteSelected;
+    public GameObject piecesSelected;
+
+    void Start() {
+
+        piecesSelected = GameObject.FindGameObjectWithTag("PiecesSelected");
+
+        spriteSelected = piecesSelected.gameObject.GetComponent<Pieces>().sprites.ToArray();
+
+        sprites = new Sprite[spriteSelected.Length];
+        for (int i = 0; i < spriteSelected.Length; i++) {
+            transform.GetChild(i).gameObject.GetComponent<SpriteRenderer>().sprite = spriteSelected[i];
+        }
+    }
+
+    void Update() {
+
+    }
+}
