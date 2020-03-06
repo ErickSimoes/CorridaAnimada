1930ec038964e769bd0da3e4eddb81343a0ecc31
Erick Simões <erick.simoes@live.com>
2020-03-06 14:55:17 -0300

    Add pieces dynamic on CM Target Group
    Resolves: #9

---
+++ Assets/Scripts/TargetGroupController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetGroupController : MonoBehaviour {
+
+    public GameObject piecesGroup;
+
+    void Start() {
+        Cinemachine.CinemachineTargetGroup targetGroup = GetComponent<Cinemachine.CinemachineTargetGroup>();
+        int numPieces = piecesGroup.transform.childCount;
+        int radius = 2;
+        for (int i = 0; i < numPieces; i++) {
+            if (i == numPieces - 1) {
+                radius = 10;
+            }
+            targetGroup.AddMember(piecesGroup.transform.GetChild(i), 1, radius);
+        }
+
+    }
+}
